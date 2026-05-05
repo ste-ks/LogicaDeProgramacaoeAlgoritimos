565750c2ad3a4af4dcd824a796b0bcab95e0160f
stefano <ste_kim@hotmail.com>
2026-05-04 22:47:18 -0300

    clear prog aula2 pratica

--- prog/aula2_Pratica/exercicio1.py
+++
@@ -1,3 +0,0 @@
-valor_Produto = float(input('qual valor do produto: '))
-valor_Desconto = float(input('qual valor do desconto: '))
-print(f'valor com desconto é {valor_Produto - (valor_Produto * valor_Desconto / 100.0)}, o desconto é de {valor_Desconto}% e você economiza {(valor_Produto * valor_Desconto / 100.0)} do original de {valor_Produto}')
--- prog/aula2_Pratica/exercicio2.py
+++
@@ -1,9 +0,0 @@
-kmDrived = float(input("qual a km percorrida: "))
-rentDays = int(input("quantos dias de aluguel: "))
-'''
-60 reais dia
-0.15 / km
-'''
-totalAmount = (kmDrived * 0.15)+(rentDays * 60)
-print(f'a kilometragem foi de {kmDrived}km, diaria total de {rentDays}')
-print(f'valor de dirarias de {rentDays * 60}R$ e valor total por quilometragem de {kmDrived * 0.15} R$ totalizando {totalAmount}')
