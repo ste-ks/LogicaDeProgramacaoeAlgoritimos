c7ec14a0cd21e9df9b2f0c535bd9156bc3cba54a
stefano <ste_kim@hotmail.com>
2026-05-04 21:18:01 -0300

    ulpload all initial files

---
+++ prog/aula2_Pratica/main.py
@@ -0,0 +1,63 @@
+import math
+
+# print(1+2+3+4+5)
+
+# print((23+19+31)/3)
+
+# print(403//73)
+
+# print(403%73)
+
+# print(2**10)
+
+# print(math.fabs((54- 57)))
+# print(abs(54- 57))
+
+
+
+# def maior(a,b,c):
+#     m = (a,b,c)
+#     return max(m)
+# def menor(a,b,c):
+#     m = (a,b,c)
+#     return min(m)
+# a = int(input('Digite um número: '))
+# b = int(input('Digite um número: '))
+# c = int(input('Digite um número: '))
+# print(f'O maior valor é {maior(a,b,c)}.')
+# print(f'O menor valor é {menor(a,b,c)}.')
+# ----------------------
+# x= float(input('digite o valor de x: '))
+# y= float(input('digite o valor de y: '))
+# z= float(input('digite o valor de z: '))
+# def max_Val(x,y,z):
+#     m = [x,y,z]
+#     return max(m)
+# def min_Val(x,y,z):
+#     m = [x,y,z]
+#     return min(m)
+# print(f'maior valor é {max_Val(x,y,z)}')
+# print(f'menor valor é {min_Val(x,y,z)}')
+# print(f'maior valor é {max(x,y,z)}')
+# print(f'menor valor é {min(x,y,z)}')
+
+
+# print(f'maior valor é {max(34,29,31)}')
+# print(f'menor valor é {min(34,29,31)}')
+
+
+# a=3
+# b=4
+# c=a*a+b*b
+# print(a , ' ' ,b , ' ' ,c)
+
+
+s1 = 'ant'
+s2 = 'bat'
+s3 = 'cod'
+# res = s1+' '+s2+' '+s3
+# res = f'{(s1 + ' ') * 10}'
+# res = f'{(s1 + ' ') * 1}{(s2 + ' ') * 2}{(s3 + ' ') * 3}'
+# res = (s1+' '+s2+' ')*7
+res = f'{(s2) * 2}{s3} '*4
+print(res)
